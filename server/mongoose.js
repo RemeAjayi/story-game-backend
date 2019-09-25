39896ecad1de2402739a9c82dc43c9862f4e15e1
Olohireme <oajayi@seamfix.com>
2019-09-26 00:14:51 +0100

    prep for deploy

--- server/mongoose.js
+++ server/mongoose.js
@@ -1,6 +1,6 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://127.0.0.1:27017/story-game-api';
+const mongoDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/story-game-api';
 mongoose.set('useFindAndModify', false);
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 const db = mongoose.connection;
