3b9bbe092a15b9bf30b39414f87ad25ea1d0d847
RemeAjayi <remeeajayi@gmail.com>
2020-08-24 08:15:12 +0100

    remove comments

--- server/server.js
+++ server/server.js
@@ -39,13 +39,6 @@ app.use(express.json())
 
 app.use(cors())
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
-
 /* ROUTES FOR PLAYERS */
 // create new player
 app.post('/player', async (req, res) => {
