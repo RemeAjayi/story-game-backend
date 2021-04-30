19f35440d0f6643bed2c93acad7acd32782291f5
Reme <remeajayi@gmail.com>
2021-04-29 22:01:56 -0400

    edits to mongoose

--- server/mongoose.js
+++ server/mongoose.js
@@ -1,23 +1,12 @@
 //Set up mongoose connection
 
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://admin:uQ4kMp4CVTpipiH3@cluster0.qnwcp.mongodb.net/story-game-api?retryWrites=true&w=majority";
-const uri = process.env.MONGODB_URI;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-/*client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});*/
-
 /* include local mongoose connection later*/
 const mongoose = require('mongoose');
-// const mongoDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/story-game-api';
-const mongoDB = uri || 'mongodb://127.0.0.1:27017/story-game-api';
+const mongoDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/story-game-api';
 mongoose.set('useFindAndModify', false);
-mongoose.connect(mongoDB,  {
-    useCreateIndex: true,
-    useNewUrlParser: true
-});
+mongoose
+     .connect( mongoDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+     .then(() => console.log( 'Database Connected' ))
+     .catch(err => console.log( err ));
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
